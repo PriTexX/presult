4ca9fbdf5f68b9778f51a2abbe04921291a645b4
pritexx <vladon_2004@mail.ru>
2023-11-04 12:07:40 +0300

    init

---
+++ Result/Result.cs
@@ -0,0 +1,72 @@
+﻿namespace Result;
+
+public readonly struct Result<TValue> : IEquatable<Result<TValue>>
+{
+    private enum ResultState
+    {
+        Success,
+        Failure,
+    }
+
+    private readonly ResultState _state;
+    private readonly TValue _value;
+    private readonly Exception _error;
+
+    public Result(TValue value)
+    {
+        _state = ResultState.Success;
+        _value = value;
+        _error = default!;
+    }
+
+    public Result(Exception error)
+    {
+        _state = ResultState.Failure;
+        _value = default!;
+        _error = error ?? throw new ArgumentNullException(nameof(error));
+    }
+
+    public bool IsSuccess => _state == ResultState.Success;
+    public bool IsError => _state == ResultState.Failure;
+
+    public TRes Match<TRes>(Func<TValue, TRes> success, Func<Exception, TRes> fail) =>
+        IsSuccess ? success(_value) : fail(_error);
+
+    public TValue UnsafeValue =>
+        IsError ? throw new ArgumentException("Trying to access value in Failure state") : _value;
+
+    public Exception UnsafeError =>
+        IsSuccess ? throw new ArgumentException("Trying to access error in Success state") : _error;
+
+    public static implicit operator Result<TValue>(TValue value) => new(value);
+
+    public static implicit operator Result<TValue>(Exception error) => new(error);
+
+    public bool Equals(Result<TValue> other)
+    {
+        return _state == other._state
+               && EqualityComparer<TValue>.Default.Equals(_value, other._value)
+               && _error.Equals(other._error);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Result<TValue> other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine((int)_state, _value, _error);
+    }
+
+    public static bool operator ==(Result<TValue> left, Result<TValue> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Result<TValue> left, Result<TValue> right)
+    {
+        return !(left == right);
+    }
+}
+
