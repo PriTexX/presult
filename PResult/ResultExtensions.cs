bad83ff973375840340eb236fe41541e36b883a3
pritexx <vladon_2004@mail.ru>
2024-01-15 21:50:10 +0300

    removed trash ToAsyncResult methods

--- PResult/ResultExtensions.cs
+++ PResult/ResultExtensions.cs
@@ -6,20 +6,4 @@ public static class ResultExtensions
     {
         return task;
     }
-
-    public static AsyncResult<T> ToAsyncResult<T>(this Task<T> task)
-    {
-        return task.ContinueWith<Result<T>>(
-            finishedTask =>
-                finishedTask.Exception is not null ? finishedTask.Exception : finishedTask.Result
-        );
-    }
-
-    public static AsyncResult<Unit> ToAsyncResult(this Task task)
-    {
-        return task.ContinueWith<Result<Unit>>(
-            finishedTask =>
-                finishedTask.Exception is not null ? finishedTask.Exception : Unit.Default
-        );
-    }
 }
